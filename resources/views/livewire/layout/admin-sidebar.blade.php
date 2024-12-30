26307b1e3bf33e4481ba61709aa28a204f547874
Zackyyhaa <zackyhafsa089@gmail.com>
2024-12-30 18:47:37 +0700

    add navbar active link on admin sidebar

--- resources/views/livewire/layout/admin-sidebar.blade.php
+++ resources/views/livewire/layout/admin-sidebar.blade.php
@@ -8,15 +8,16 @@ new class extends Component {}; ?>
 <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
     <ul class="space-y-2 font-medium">
         <li>
-            <a href="{{ route('admin.request') }}"
-                class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+            <a href="{{ route('admin.request') }}" class="flex items-center p-2 rounded-lg group
+            {{ Route::is('admin.request') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700' }}">
+                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 
+                    {{ Route::is('admin.request') ? 'text-blue-600' : 'group-hover:text-gray-900 dark:group-hover:text-white' }}" 
                     aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                    <path
-                        d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
+                    <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                 </svg>
-                <span class="flex-1 ms-3 whitespace-nowrap">Permintaan Surat</span>
+                <span class="flex-1 ml-3 whitespace-nowrap">Permintaan Surat</span>
             </a>
+
         </li>
         <li>
             <button type="button"
@@ -39,66 +40,71 @@ new class extends Component {}; ?>
             <ul id="dropdown-layouts" class="hidden py-2 space-y-2">
                 <li>
                     <a href="{{ route('admin.poverty-letter') }}"
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Surat
+                        class="flex items-center p-2 text-base transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700
+                        {{ Route::is('admin.poverty-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">
+                        Surat
                         Keterangan Tidak Mampu</a>
                 </li>
                 <li>
                     <a href="{{ route('admin.moving-one-village-letter') }}"
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Surat
+                        class="flex items-center p-2 text-base transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 {{ Route::is('admin.moving-one-village-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">Surat
                         Pindah Satu Desa</a>
                 </li>
                 <li>
                     <a href="{{ route('admin.domicile-letter') }}"
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">
+                        class="flex items-center p-2 text-base transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700
+                        {{ Route::is('admin.domicile-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">
                         Surat Domisili
                     </a>
                 </li>
                 <li>
                     <a href="{{ route('admin.bussines-letter') }}"
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Surat
+                        class="flex items-center p-2 text-base transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 {{ Route::is('admin.bussines-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">Surat
                         Keterangan Usaha</a>
                 </li>
                 <li>
                     <a href="{{ route('admin.birth-letter') }}  "
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Akta
+                        class="flex items-center p-2 text-base transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 {{ Route::is('admin.birth-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">Akta
                         Kelahiran</a>
                 </li>
                 <li>
                     <a href="{{ route('admin.village-letter') }}"
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Surat
+                        class="flex items-center p-2 text-base  transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 {{ Route::is('admin.village-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">Surat
                         Desa</a>
                 </li>
                 <li>
                     <a href=""
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Surat
+                        class="flex items-center p-2 text-base  transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 {{ Route::is('admin.poverty-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">Surat
                         Keterangan Tidak Mampu</a>
                 </li>
                 <li>
                     <a href=""
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Surat
+                        class="flex items-center p-2 text-base transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 {{ Route::is('admin.poverty-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">Surat
                         Pendapatan</a>
                 </li>
                 <li>
                     <a href=""
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Surat
+                        class="flex items-center p-2 text-base transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 {{ Route::is('admin.poverty-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">Surat
                         Keterangan Belum Menikah</a>
                 </li>
                 <li>
                     <a href=""
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Surat
+                        class="flex items-center p-2 text-base transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 {{ Route::is('admin.poverty-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">Surat
                         Pernikahan</a>
                 </li>
                 <li>
                     <a href=""
-                        class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">Surat
+                        class="flex items-center p-2 text-base transition duration-75 rounded-lg pl-11 group hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-gray-700 {{ Route::is('admin.poverty-letter') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700' }}">Surat
                         Keterangan Beda Nama</a>
                 </li>
             </ul>
         </li>
         <li>
             <a href="{{ route('admin.water') }}"
-                class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                class="flex items-center p-2 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group
+                {{ Route::is('admin.water') ? 'bg-gray-200 text-blue-600' : 'text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700' }}">
+                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 dark:group-hover:text-white
+                 {{ Route::is('admin.water') ? 'text-blue-600' : 'group-hover:text-gray-900 dark:group-hover:text-white' }}" "
                     aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                     <path
                         d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
